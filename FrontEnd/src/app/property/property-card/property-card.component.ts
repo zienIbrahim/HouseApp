7d5b20a933d13db3c110ca2d96ebbc5133e6ee41
gasimyahia <gasimyya@gmail.com>
2022-02-13 23:00:26 +0200

    initial comment

--- FrontEnd/src/app/property/property-card/property-card.component.ts
+++ FrontEnd/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -7,12 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyCardComponent implements OnInit {
 
-  Property:any ={
-    "Id":1,
-    "Name":"Gasim House",
-    "Type":"House",
-    "Price":1200
-  }
+  @Input() property :any;
+
   constructor() { }
 
   ngOnInit(): void {
